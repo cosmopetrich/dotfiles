fb6b12baedc4020ad51f5752b976ed75ba42afad
Cosmo Petrich <cosmo.petrich@invoice2go.com>
2016-08-24 10:59:25 +1000

    Update zsh config

--- zsh/zshrc.d/pyenv.zsh
+++
@@ -1,5 +0,0 @@
-PYENV_BIN="${HOME}/.pyenv/bin"
-if [ -f "$PYENV_BIN" ]; then
-	export PATH="${PYENV_BIN}:${PATH}"
-	eval "$(pyenv init -)"
-fi
--- zsh/zshrc.d/rbenv.zsh
+++
@@ -1,5 +0,0 @@
-RBENV_BIN="${HOME}/.rbenv/bin"
-if [ -f "$RBENV_BIN" ]; then
-	export PATH="${RBENV_BIN}:${PATH}"
-	eval "$(rbenv init -)"
-fi
